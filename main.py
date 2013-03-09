7ffbc45733e14e4776862a961175c561942ed071
bill7cao <bill7cao@berkeley.edu>
2013-03-09 09:12:22 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,11 @@ this file depends on course_info.py , student_info.py
 the algorithm and communication with the frontend should be done here
 '''
 
-import course_info.*
-import student_info.*
+from ap_info import *
+from student_info import *
+from course_info import *
+from algorithm import *
+from app.py import *
+
+def make_demo(student):
+    return generate_schedule(algorithm)
